ea42fdb008eed063b1eb4104004064f01f386b19
Alina <AZharkevich@sportmasterlab.net>
2023-10-11 09:57:37 +0500

    первая домашка

---
+++ src/first_homework/Main.java
@@ -0,0 +1,14 @@
+package first_homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(GameUtils.throwFourSideDice());
+        System.out.println(GameUtils.throwSixSideDice());
+        System.out.println(GameUtils.throwEightSideDice());
+        System.out.println(GameUtils.throwTwelveSideDice());
+        System.out.println(GameUtils.throwTwentySideDice());
+        System.out.println(GameUtils.throwTenSideDice());
+        System.out.println(GameUtils.throwDoubleTenSideDice());
+    }
+}
